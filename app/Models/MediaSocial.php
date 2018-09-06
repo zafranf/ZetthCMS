db99e2a4e6158e09bc65e5a2f7967b9dee67ecad
zafranf <zafranfadilah@gmail.com>
2018-09-06 10:48:09 +0700

    tambah softdeletes

--- app/Models/MediaSocial.php
+++ app/Models/MediaSocial.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MediaSocial extends Model
 {
-    //
+    use SoftDeletes;
 }
