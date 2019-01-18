8d6189a0b3a95a7abce156d66ea844d096760a2c
zafranf <zafranfadilah@gmail.com>
2019-01-19 01:45:12 +0700

    separate topbar

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -2,20 +2,20 @@
 <html>
 
 <head>
-  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-  <meta charset="utf-8" />
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  <meta charset="utf-8">
   <title>Pages - Admin Dashboard UI Kit - Blank Page</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
   {{-- <link rel="apple-touch-icon" href="pages/ico/60.png">
   <link rel="apple-touch-icon" sizes="76x76" href="pages/ico/76.png">
   <link rel="apple-touch-icon" sizes="120x120" href="pages/ico/120.png">
   <link rel="apple-touch-icon" sizes="152x152" href="pages/ico/152.png"> --}}
-  <link rel="icon" type="image/x-icon" href="{{ _get_image('/admin/images/' . $apps->icon) }}" />
+  <link rel="icon" type="image/x-icon" href="{{ _get_image('/admin/images/' . $apps->icon) }}">
   {{-- <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-touch-fullscreen" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
-  <meta content="" name="description" />
-  <meta content="" name="author" /> --}}
+  <meta content="" name="description">
+  <meta content="" name="author"> --}}
   <link href="{{ url(mix('/admin/css/app.css')) }}" rel="stylesheet">
   @yield('styles')
 </head>
