5983d35dc18a7771af350ac2d69a8d7aaa140192
zafranf <zafranfadilah@gmail.com>
2019-09-15 17:20:01 +0700

    lanjut frontend

--- app/Http/Controllers/Site/MainConroller.php
+++ app/Http/Controllers/Site/MainConroller.php
@@ -9,7 +9,17 @@ class MainController extends Controller
 {
     public function index(Request $r)
     {
+        /* get all data */
+        $data = [
+            'banners' => \ZetthCore\Models\Banner::where('status', 1)->orderBy('order')->get(),
+            'posts' => \ZetthCore\Models\Post::where('status', 1)->where('type', 'article')->orderBy('published_at', 'desc')->take(5)->get(),
+            'categories' => \ZetthCore\Models\Term::where('status', 1)->where('type', 'category')->get(),
+            'tags' => \ZetthCore\Models\Term::where('status', 1)->where('type', 'tag')->get(),
+            'albums' => \ZetthCore\Models\Album::where('status', 1)->where('type', 'photo')->get(),
+            'videos' => \ZetthCore\Models\Post::where('status', 1)->where('type', 'video')->orderBy('published_at', 'desc')->take(5)->get(),
+        ];
 
+        return view('site.main', $data);
     }
 
     public function contact(Request $r)
