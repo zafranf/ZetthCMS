f2b9f331855a65a72edbd26141c92aa5560e1d51
zafranf <zafranfadilah@gmail.com>
2020-02-05 02:52:04 +0700

    refine filemanager

--- app/Http/Controllers/Site/PostController.php
+++ app/Http/Controllers/Site/PostController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Site;
 
+use App\Http\Controllers\Site\Controller;
 use Illuminate\Http\Request;
-use ZetthCore\Http\Controllers\SiteController;
 
-class PostController extends SiteController
+class PostController extends Controller
 {
     public function __construct()
     {
