e062ee872416925c21db183c21edfa7d7666cc7c
zafranf <zafranfadilah@gmail.com>
2020-02-12 11:59:27 +0700

    remove double route name

--- routes/site.php
+++ routes/site.php
@@ -1,6 +1,6 @@
 <?php
 /* Site Routes */
-Route::name('web.')->middleware(['site'])->group(function () {
+Route::middleware(['site'])->group(function () {
     /* Log all visits */
     Route::middleware(['visitor_log'])->group(function () {
 
