f5cfb93c7eef50c305a127b9debc63acd57bb719
zafranf <zafranfadilah@gmail.com>
2020-04-29 05:28:07 +0700

    fix routes..

--- routes/site.php
+++ routes/site.php
@@ -1,8 +1,8 @@
 <?php
 /* Site Routes */
-Route::middleware(['site'])->group(function () {
-    /* Log all visits */
-    Route::middleware(['visitor_log'])->group(function () {
-        //
-    });
+//
+
+/* Log all visits */
+Route::middleware(['visitor_log'])->group(function () {
+    //
 });
--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,10 @@ Route::name('web.')->middleware(['site'])->group(function () {
         Route::post('/verification/resend', 'Auth\VerificationController@resend')->name('verify.resend.post');
         Route::post('/forgot-password', 'Auth\ForgotPasswordController@send')->name('forgot.post');
         Route::post('/reset-password', 'Auth\ResetPasswordController@store')->name('reset.post');
-        Route::post('/keluar', 'Auth\LoginController@logout')->name('logout.post');
+        Route::post('/logout', 'Auth\LoginController@logout')->name('logout.post');
     });
     if (env('APP_DEBUG')) {
-        Route::get('/keluar', 'Auth\LoginController@logout')->name('logout');
+        Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
         Route::get('/test', 'TestController@index')->name('test');
     }
 
@@ -45,6 +45,52 @@ Route::name('web.')->middleware(['site'])->group(function () {
         Route::get('/share/{slug}/{socmed}', 'Site\ActionController@share')->name('share');
     });
 
+    /* POST */
+    Route::post('/profile', 'Site\UserController@update')->name('profile.post');
+
+    /* Log all visits */
+    Route::middleware(['visitor_log'])->group(function () {
+        /* check login status */
+        Route::middleware(['guest'])->group(function () {
+            Route::get('/' . env('REGISTER_PATH', 'register'), 'Auth\LoginController@showRegistrationForm')->name('register');
+            Route::get('/' . env('LOGIN_PATH', 'login'), 'Auth\LoginController@showLoginForm')->name('login');
+            Route::get('/' . env('VERIFICATION_PATH', 'verification') . '/{type}', 'Auth\VerificationController@index')->name('verify');
+            Route::get('/' . env('FORGOTPASSWORD_PATH', 'forgot-password'), 'Auth\ForgotPasswordController@index')->name('forgot.password');
+            Route::get('/' . env('RESETPASSWORD_PATH', 'reset-password'), 'Auth\ResetPasswordController@index')->name('reset.password');
+        });
+
+        /* must logged in */
+        Route::middleware(['auth'])->group(function () {
+            /* GET */
+            Route::get('/' . env('PROFILE_PATH', 'profile'), 'Site\UserController@index')->name('profile');
+            Route::get('/' . env('PROFILE_PATH', 'profile') . '/edit', 'Site\UserController@edit')->name('profile.edit');
+        });
+
+        /* Front Routes */
+        Route::get('/', 'Site\MainController@index')->name('root');
+        Route::get('/' . env('search_PATH', 'search'), 'Site\MainController@search')->name('search');
+
+        /* Gallery Routes */
+        Route::get('/' . env('ALBUMS_PATH', 'albums'), 'Site\GalleryController@album')->name('albums');
+        Route::get('/' . env('ALBUM_PATH', 'album') . '/{slug}', 'Site\GalleryController@album_detail')->name('album');
+        Route::get('/' . env('VIDEOS_PATH', 'videos'), 'Site\GalleryController@video')->name('videos');
+        Route::get('/' . env('VIDEO_PATH', 'video') . '/{slug}', 'Site\GalleryController@video_detail')->name('video');
+
+        /* Misc Routes */
+        Route::get('/' . env('COMINGSOON_PATH', 'comingsoon'), 'Site\MiscController@comingsoon')->name('comingsoon');
+        Route::get('/' . env('MAINTENANCE_PATH', 'maintenance'), 'Site\MiscController@maintenance')->name('maintenance');
+
+        /* Posts by Routes */
+        Route::get('/{by_type}/{slug}', 'Site\PostController@by')->name('posts.by')->where('by_type', env('CATEGORY_PATH', 'category') . '|' . env('TAG_PATH', 'tag') . '|' . env('AUTHOR_PATH', 'author'));
+
+        /* Posts Routes */
+        Route::get('/{type}', 'Site\PostController@index')->name('posts')->where('type', env('POSTS_PATH', 'posts'));
+        Route::get('/{type}/{slug}', 'Site\PostController@detail')->name('post')->where('type', env('POST_PATH', 'post'));
+
+        /* Page Route */
+        Route::get(env('CONTACT_PATH', 'contact'), 'Site\PageController@contact')->name('contact');
+    });
+
     /* include additional menu site */
     $siteRoute = base_path('routes') . '/site.php';
     if (file_exists($siteRoute)) {
@@ -53,46 +99,7 @@ Route::name('web.')->middleware(['site'])->group(function () {
 
     /* Log all visits */
     Route::middleware(['visitor_log'])->group(function () {
-        /* check login status */
-        Route::middleware(['guest'])->group(function () {
-            Route::get('/daftar', 'Auth\LoginController@showRegistrationForm')->name('register');
-            Route::get('/masuk', 'Auth\LoginController@showLoginForm')->name('login');
-            Route::get('/verifikasi/{type}', 'Auth\VerificationController@index')->name('verify');
-            Route::get('/lupa-sandi', 'Auth\ForgotPasswordController@index')->name('forgot.password');
-            Route::get('/ubah-sandi', 'Auth\ResetPasswordController@index')->name('reset.password');
-        });
-
-        /* must logged in */
-        Route::middleware(['auth'])->group(function () {
-            Route::post('/profil', 'Site\UserController@update')->name('profile.post');
-
-            Route::get('/profil', 'Site\UserController@index')->name('profile');
-            Route::get('/profil/edit', 'Site\UserController@edit')->name('profile.edit');
-        });
-
-        /* Front Routes */
-        Route::get('/', 'Site\MainController@index')->name('root');
-        Route::get('/cari', 'Site\MainController@search')->name('search');
-
-        /* Gallery Routes */
-        Route::get('/albums', 'Site\GalleryController@album')->name('albums');
-        Route::get('/album/{slug}', 'Site\GalleryController@album_detail')->name('album');
-        Route::get('/videos', 'Site\GalleryController@video')->name('videos');
-        Route::get('/video/{slug}', 'Site\GalleryController@video_detail')->name('video');
-
-        /* Misc Routes */
-        Route::get('/comingsoon', 'Site\MiscController@comingsoon')->name('comingsoon');
-        Route::get('/maintenance', 'Site\MiscController@maintenance')->name('maintenance');
-
-        /* Posts by Routes */
-        Route::get('/{by_type}/{slug}', 'Site\PostController@by')->name('posts.by')->where('by_type', 'category|tag|author|kategori|label|penulis');
-
-        /* Posts Routes */
-        Route::get('/{type}', 'Site\PostController@index')->name('posts')->where('type', 'posts|articles|news|artikel|berita');
-        Route::get('/{type}/{slug}', 'Site\PostController@detail')->name('post')->where('type', 'post|article|news|baca-artikel|baca-berita');
-
         /* Page Route */
-        Route::get('/kontak', 'Site\PageController@contact')->name('contact');
         Route::get('/{slug}', 'Site\PageController@index')->name('page');
     });
 });
