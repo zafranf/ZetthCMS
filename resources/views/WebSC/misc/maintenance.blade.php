5d71fb51c1fc9a5a947526577701d37de77e4784
zafranf <zafranfadilah@gmail.com>
2020-05-05 23:25:45 +0700

    refine layout misc

--- resources/views/WebSC/misc/maintenance.blade.php
+++ resources/views/WebSC/misc/maintenance.blade.php
@@ -1,65 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ app('site')->lang }}">
+@extends('WebSC.misc.main')
 
-<head>
-  {!! SEOMeta::generate() !!}
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  @if (!isset($status_code))
-    <meta name="robots" content="index, follow">
-  @else
-    <meta name="robots" content="none">
-  @endif
-  {{-- Open Graph SEO --}}
-  {!! OpenGraph::generate() !!}
-  @if (!empty($page_title))
-    {!! Twitter::generate() !!}
-  @endif
-  <meta name="theme-color" content="#df342f">
-  <link rel="shortcut icon" href="{{ getImageLogo(app('site')->icon) }}" type="image/x-icon">
-  <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-  <style>
-    html,
-    body {
-        height: 100%;
-    }
-    
-    body {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        display: table;
-        font-weight: 100;
-        font-family: 'Lato';
-    }
-    
-    .container {
-        text-align: center;
-        display: table-cell;
-        vertical-align: middle;
-    }
-    
-    .content {
-        text-align: center;
-        display: inline-block;
-    }
-    
-    .title {
-        font-size: 56px;
-    }
-  </style>
-</head>
-
-<body>
+@section('content')
   <div class="container">
     <div class="content">
-    <div class="title">Maaf, situs sedang dalam perbaikan...</div>
+      <div class="logo">
+        <img src="{{ getImageLogo('logo/landscape.png') }}" width="100%">
+      </div>
+      <div class="title">Maaf, situs sedang dalam perbaikan...</div>
     </div>
   </div>
-  
-  @include('google.analytics')
-  <!--[if]></body></html><![endif]-->
-</body>
-
-</html>
+@endsection
